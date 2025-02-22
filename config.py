a3c2e1f49252258cc75d51f557064de55555aa51
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-02-22 13:11:32 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os  
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-    API_ID = int(os.getenv("API_ID", "YOUR_API_ID_HERE"))
-    API_HASH = os.getenv("API_HASH", "YOUR_API_HASH_HERE")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-100XXXXXXXXX"))  # চ্যানেল আইডি দিন
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7968513647:AAHhDKQ0EgxoOIGj43GIndNFPNDYaCzSAsQ")
+    API_ID = int(os.getenv("API_ID", "27148454"))
+    API_HASH = os.getenv("API_HASH", "f668c20d77d1a8feee31afdc810f8ac4")
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1002011482617"))  # চ্যানেল আইডি দিন
